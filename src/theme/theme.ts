9fd5dc663ef64539701938bd617b14dbd14e5a3c
Denisalik <schegletovdenis@mail.ru>
2022-03-26 19:00:26 +0300

    added imports

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,7 +1,9 @@
 // import
 
 import { createTheme } from "@mui/material";
+import { palette } from "./palette";
+import { typography } from "./typography";
 
-const theme = createTheme({});
+const theme = createTheme({palette, typography});
 
 export default theme;
