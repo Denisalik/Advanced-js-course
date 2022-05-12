cf7935ba0dd27b32624cd86b2ec4f7f2fb93e747
Denisalik <schegletovdenis@mail.ru>
2022-05-12 13:24:50 +0300

    report

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,14 +1,15 @@
 /** @type {import("ts-jest/dist/types").InitialOptionsTsJest} */
 module.exports = {
-  // preset: "ts-jest",
-  // testEnvironment: "node",
-  // transform: {
-  //   "^.+\\.tsx?$": "ts-jest",
-  //   "^.+\\.jsx?$": "babel-jest",
-  // },
-  // transformIgnorePatterns: ["node_modules/.*"],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.[j|t]sx?$": "babel-jest",
+    "^.+\\.tsx?$": "ts-jest",
+    // "\\.tsx?$": "ts-jest",
+    // "\\.jsx?$": "babel-jest",
+  },
+  transformIgnorePatterns: ["<rootDir>/__test__./*./","node_modules/@mui/", "node_modules/@ijl/.*"],
   //this shows another error
-  //syntax https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Assertions
   // transform: {
   //   "node_modules/@mui/.+\\.(j|t)sx?$": "ts-jest",
   // },
@@ -17,11 +18,24 @@ module.exports = {
   //   "^@ijl/cli$": "<rootDir>/ijl.cli.js",
   // },
   testMatch: ["<rootDir>/__test__/*.(test|spec).(ts|tsx)"],
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-  },
-  testEnvironment: "jsdom",
-};
+  // transform: {
+  //   "^.+\\.tsx?$": "ts-jest",
+  // },
+  // testEnvironment: "jsdom",
 
+  verbose: true,
+  collectCoverage: false,
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  collectCoverageFrom: [
+    "./src/**/*.ts?(x)",
+  ],
+  coverageDirectory: "<rootDir>/reports/coverage",
+  coverageReporters: [
+    [
+      "html",
+      { subdir: "html" },
+    ],
+  ],
+};
 //80% and at least 1 ui test
 // https://javascript.plainenglish.io/the-practical-guide-to-start-react-testing-library-with-typescript-d386804a018
