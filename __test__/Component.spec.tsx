fe12c230b746928c7a4d314f3ff5d2c459e5bc78
Denisalik <schegletovdenis@mail.ru>
2022-05-12 15:24:46 +0300

    fixed error with jsdom

--- __test__/Component.spec.tsx
+++ __test__/Component.spec.tsx
@@ -3,10 +3,15 @@ import { render, RenderResult, cleanup } from "@testing-library/react";
 import { renderProviders } from "./test-utils";
 import Auth from "../src/components/auth/Auth";
 import "@testing-library/jest-dom";
+import Home from "../src/components/home/Home";
 
 let documentBody: RenderResult;
+jest.useFakeTimers();
 
 describe("Sign in", () => {
+  afterAll(() => {
+    jest.runAllTimers();
+  });
   beforeEach(cleanup);
   it("jest config test", () => {});
   it("shows message", () => {
@@ -16,5 +21,9 @@ describe("Sign in", () => {
         "If you already have an account, login to invite a friend to play with you."
       )
     ).toBeInTheDocument();
+  });
+  it("shows message connect", () => {
+    documentBody = render(renderProviders(<Home />));
+    expect(documentBody.getByText("Connect")).toBeInTheDocument();
   });
 });
--- jest.config.cjs
+++ jest.config.cjs
@@ -7,8 +7,7 @@ module.exports = {
     "^.+\\.(t|j)sx?$": "ts-jest",
   },
   transformIgnorePatterns: [
-    // Change MODULE_NAME_HERE to your module that isn't being compiled
-    // "/node_modules/(?!@mui).+\\.js$",
+    // If you have error about imports from node_modules, that means they are using ESM and you need to ignore them.
     "node_modules/(?!(@mui|@babel)/)"
   ],
   moduleNameMapper: {
@@ -17,7 +16,6 @@ module.exports = {
   testMatch: ["<rootDir>/__test__/*.(test|spec).(ts|tsx)"],
   verbose: true,
   collectCoverage: false,
-  // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: [
     "./src/**/*.ts?(x)",
   ],
@@ -30,4 +28,3 @@ module.exports = {
   ],
 };
 //80% and at least 1 ui test
-// https://javascript.plainenglish.io/the-practical-guide-to-start-react-testing-library-with-typescript-d386804a018
